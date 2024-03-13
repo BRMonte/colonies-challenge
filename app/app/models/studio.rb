e1e216e18e0fe7e8f71c2713ea482f2f0fe7a2fa
Bruno Montebello <brunormontebello@hotmail.com>
2024-03-13 02:11:20 -0300

    Feat: add Studio model validations + specs

--- app/app/models/studio.rb
+++ app/app/models/studio.rb
@@ -1,2 +1,5 @@
 class Studio < ApplicationRecord
+  has_many :stays
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/spec/models/studio_spec.rb
+++ app/spec/models/studio_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Studio, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'validates presence of name' do
+      valid_studio = build(:studio, name: 'test')
+      nameless_studio = build(:studio, name: nil)
+
+      expect(nameless_studio).not_to be_valid
+      expect(nameless_studio.errors[:name]).to include("can't be blank")
+      expect(valid_studio).to be_valid
+    end
+
+    it 'validates uniqueness of name' do
+      create(:studio, name: 'Test Studio')
+      duplicated_studio = build(:studio, name: 'Test Studio')
+
+      expect(duplicated_studio).not_to be_valid
+      expect(duplicated_studio.errors[:name]).to include("has already been taken")
+    end
+  end
+
+  describe 'associations' do
+    it 'has many stays' do
+      association = described_class.reflect_on_association(:stays)
+      expect(association.macro).to eq :has_many
+    end
+  end
 end
