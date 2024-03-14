d819e3ba5a56f1b751e3918dc84fe82a14906175
Bruno Montebello <brunormontebello@hotmail.com>
2024-03-14 16:00:29 -0300

    Uncomment spec file

--- app/spec/models/studio_spec.rb
+++ app/spec/models/studio_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Studio, type: :model do
-  # describe 'validations' do
-  #   it 'validates presence of name' do
-  #     valid_studio = build(:studio, name: 'test')
-  #     nameless_studio = build(:studio, name: nil)
+  describe 'validations' do
+    it 'validates presence of name' do
+      valid_studio = build(:studio, name: 'test')
+      nameless_studio = build(:studio, name: nil)
 
-  #     expect(nameless_studio).not_to be_valid
-  #     expect(nameless_studio.errors[:name]).to include("can't be blank")
-  #     expect(valid_studio).to be_valid
-  #   end
+      expect(nameless_studio).not_to be_valid
+      expect(nameless_studio.errors[:name]).to include("can't be blank")
+      expect(valid_studio).to be_valid
+    end
 
-  #   it 'validates uniqueness of name' do
-  #     create(:studio, name: 'Test Studio')
-  #     duplicated_studio = build(:studio, name: 'Test Studio')
+    it 'validates uniqueness of name' do
+      create(:studio, name: 'Test Studio')
+      duplicated_studio = build(:studio, name: 'Test Studio')
 
-  #     expect(duplicated_studio).not_to be_valid
-  #     expect(duplicated_studio.errors[:name]).to include("has already been taken")
-  #   end
-  # end
+      expect(duplicated_studio).not_to be_valid
+      expect(duplicated_studio.errors[:name]).to include("has already been taken")
+    end
+  end
 
-  # describe 'associations' do
-  #   it 'has many stays' do
-  #     association = described_class.reflect_on_association(:stays)
-  #     expect(association.macro).to eq :has_many
-  #   end
-  # end
+  describe 'associations' do
+    it 'has many stays' do
+      association = described_class.reflect_on_association(:stays)
+      expect(association.macro).to eq :has_many
+    end
+  end
 
   describe 'class method' do
     describe '.absences' do
