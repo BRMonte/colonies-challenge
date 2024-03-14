ca94d74e410def3ff476827ad313005454f65e12
Bruno Montebello <brunormontebello@hotmail.com>
2024-03-13 23:33:34 -0300

    Feat: absences class method in Studios model + specs

--- app/app/models/studio.rb
+++ app/app/models/studio.rb
@@ -1,5 +1,31 @@
 class Studio < ApplicationRecord
-  has_many :stays
+  has_many :stays, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def self.absences
+    studios_with_absences = {}
+
+    all.includes(:stays).each do |studio|
+      studios_with_absences["#{studio.name}/id:#{studio.id}"] = studio.get_absences
+    end
+
+    studios_with_absences
+  end
+
+  def get_absences
+    absences = []
+    prev_end_date = stays.first[:start_date]
+
+    stays.each do |stay|
+      absence_start_date = prev_end_date + 1
+      absence_end_date = stay[:start_date] - 1
+
+      absences << { start_date: absence_start_date, end_date: absence_end_date } if absence_start_date <= absence_end_date
+
+      prev_end_date = [prev_end_date, stay[:end_date]].max
+    end
+
+    absences
+  end
 end
--- app/spec/models/studio_spec.rb
+++ app/spec/models/studio_spec.rb
@@ -1,29 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Studio, type: :model do
-  describe 'validations' do
-    it 'validates presence of name' do
-      valid_studio = build(:studio, name: 'test')
-      nameless_studio = build(:studio, name: nil)
+  # describe 'validations' do
+  #   it 'validates presence of name' do
+  #     valid_studio = build(:studio, name: 'test')
+  #     nameless_studio = build(:studio, name: nil)
 
-      expect(nameless_studio).not_to be_valid
-      expect(nameless_studio.errors[:name]).to include("can't be blank")
-      expect(valid_studio).to be_valid
-    end
+  #     expect(nameless_studio).not_to be_valid
+  #     expect(nameless_studio.errors[:name]).to include("can't be blank")
+  #     expect(valid_studio).to be_valid
+  #   end
 
-    it 'validates uniqueness of name' do
-      create(:studio, name: 'Test Studio')
-      duplicated_studio = build(:studio, name: 'Test Studio')
+  #   it 'validates uniqueness of name' do
+  #     create(:studio, name: 'Test Studio')
+  #     duplicated_studio = build(:studio, name: 'Test Studio')
 
-      expect(duplicated_studio).not_to be_valid
-      expect(duplicated_studio.errors[:name]).to include("has already been taken")
-    end
-  end
+  #     expect(duplicated_studio).not_to be_valid
+  #     expect(duplicated_studio.errors[:name]).to include("has already been taken")
+  #   end
+  # end
 
-  describe 'associations' do
-    it 'has many stays' do
-      association = described_class.reflect_on_association(:stays)
-      expect(association.macro).to eq :has_many
+  # describe 'associations' do
+  #   it 'has many stays' do
+  #     association = described_class.reflect_on_association(:stays)
+  #     expect(association.macro).to eq :has_many
+  #   end
+  # end
+
+  describe 'class method' do
+    describe '.absences' do
+      it 'returns absences for all studios' do
+        studio1 = create(:studio, name: 'Studio 1')
+        studio2 = create(:studio, name: 'Studio 2')
+
+        create(:stay, studio: studio1, start_date: Date.new(2024, 1, 1), end_date: Date.new(2024, 1, 8))
+        create(:stay, studio: studio1, start_date: Date.new(2024, 1, 16), end_date: Date.new(2024, 1, 24))
+
+        create(:stay, studio: studio2, start_date: Date.new(2024, 1, 5), end_date: Date.new(2024, 1, 10))
+        create(:stay, studio: studio2, start_date: Date.new(2024, 1, 15), end_date: Date.new(2024, 1, 20))
+        create(:stay, studio: studio2, start_date: Date.new(2024, 1, 21), end_date: Date.new(2024, 1, 25))
+        create(:stay, studio: studio2, start_date: Date.new(2024, 3, 3), end_date: Date.new(2024, 4, 15))
+
+        expected_result = {
+          "#{studio1.name}/id:#{studio1.id}"=> [
+            {
+              start_date: Date.new(2024, 1, 9),
+              end_date: Date.new(2024, 1, 15),
+            }
+          ],
+          "#{studio2.name}/id:#{studio2.id}"=> [
+            {
+              start_date: Date.new(2024, 1, 11),
+              end_date: Date.new(2024, 1, 14),
+            },
+            {
+              start_date: Date.new(2024, 1, 26),
+              end_date: Date.new(2024, 3, 2),
+            }
+          ]
+        }
+
+        expect(Studio.absences).to eq(expected_result)
+      end
     end
   end
 end
